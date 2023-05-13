303974b893d08619193b3a693ef4ffb9e8eab3a7
hajnor <norii.hajdu@gmail.com>
2023-05-13 20:56:23 +0200

    Updates

---
+++ precache-manifest.0e570b2be870f969e45829360c705e92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "41ee46c4e774d8bbfa8a",
+    "url": "/robofriends/static/css/main.440d47e6.chunk.css"
+  },
+  {
+    "revision": "41ee46c4e774d8bbfa8a",
+    "url": "/robofriends/static/js/main.41ee46c4.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/robofriends/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "65213df16a62e798fe74",
+    "url": "/robofriends/static/css/2.681fe110.chunk.css"
+  },
+  {
+    "revision": "65213df16a62e798fe74",
+    "url": "/robofriends/static/js/2.65213df1.chunk.js"
+  },
+  {
+    "revision": "5c634a928a4eadcd2cfd",
+    "url": "/robofriends/static/js/runtime~main.5c634a92.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "423fca0aee3ac736b22b4d0b5b92be01",
+    "url": "/robofriends/index.html"
+  }
+];
